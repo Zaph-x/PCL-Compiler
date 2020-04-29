da0b254ba01699f0492c40b213b1ac9390ed6b15
Zaph-x <mikkel@luceartfoto.dk>
2020-04-30 00:19:23 +0200

    initial commit

---
+++ main.ps1
@@ -0,0 +1,13 @@
+<# 
+This is a proof of concept powershell compiler.
+This project is for fun and does not serve any real world purpose.
+
+This is main.ps1 - The main file in the compiler
+
+Zaph-x @ 29-apr 2020, 11:40 PM
+#>
+if ($args.Count -lt 1)
+{
+    Write-Error "Please provide a file to compile";
+    return 1;
+}
