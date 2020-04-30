4377a5722bb3d5e60cf41effd6283c4657d9485c
Zaph-x <mikkel@luceartfoto.dk>
2020-04-30 18:04:55 +0200

    Finished generator

---
+++ generator.ps1
@@ -0,0 +1,54 @@
+$name = $args[0].Replace(".psl", "")
+
+$template = "
+#include <stdio.h>`n
+`n
+int main(void) {`n
+%CODE%
+}
+"
+$codeblock = ""
+foreach ($token in $Global:tokens) {
+    switch ($token.Type) {
+        "KEYWORD" { 
+            if ($token.Value -eq "int") {
+                $codeblock += "    int "
+            } elseif ($token.Value -eq "float") {
+                $codeblock += "    float "
+            } elseif ($token.Value -eq "print") {
+                $codeblock += "    printf(%REPLACE%)"
+            }
+         }
+        "VAR" {
+            if ($codeblock.EndsWith("%REPLACE%)")) {
+                if (($global:symtable | where {$token.Value.Equals($_.Value)}).SymbolType -eq "INT") {
+                    $codeblock = $codeblock.Replace("%REPLACE%", """%d\n"",$($token.Value)")
+                } elseif (($global:symtable | where {$token.Value.Equals($_.Value)}).SymbolType -eq "FLOAT") {
+                    $codeblock = $codeblock.Replace("%REPLACE%", """%f\n"",$($token.Value)")
+                }
+            } else {
+                $codeblock += $token.Value
+            }
+        }
+        "ASSIGNMENT" {
+            $codeblock += "= "
+        }
+        "OPERATOR" {
+            $codeblock += "$($token.Value) "
+        }
+        "INT" {
+            $codeblock += "$($token.Value) "
+        }
+        "FLOAT" {
+            $codeblock += "$($token.Value) "
+        }
+        "ENDING" {
+            $codeblock += ";`n"
+        }
+        Default {}
+    }
+}
+$template = $template.Replace("%CODE%", $codeblock)
+$template > ".\test.c"
+gcc.exe .\test.c -o $name
+rm .\test.c
